bde6e9afced70898607498f7b9427688a53a4d94
MarcusTan <marcus.tan.598@gmail.com>
2020-08-26 21:43:07 +0800

    Rename chatbot to Botty McBotface, change greeting and exit quotes, add echo for user commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        boolean isBotRunning = true;
+        String enteredCommand;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        System.out.println("Hello, I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("Botty McBotface, at your service.");
+        System.out.println("Your wish is my command.");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (isBotRunning) {
+            enteredCommand = in.nextLine();
+            // if bye is entered, stop asking for commands and exit
+            if (enteredCommand.equals("bye")) {
+                isBotRunning = false;
+                break;
+            }
+            // repeat any other commands back to the user
+            System.out.println("____________________________________________________________");
+            System.out.println("> " + enteredCommand);
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Very well. Goodbye.");
         System.out.println("____________________________________________________________");
     }
 }
