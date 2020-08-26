81ee63fc455785d8444919a10459cbbf99948ddd
MarcusTan <marcus.tan.598@gmail.com>
2020-08-26 23:58:55 +0800

    Edit code to comply with Java coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         boolean isBotRunning = true;
-        String enteredCommand;
+        String userInput;
         Scanner in = new Scanner(System.in);
 
         Task[] taskList = new Task[100];
@@ -15,39 +15,46 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         while (isBotRunning) {
-            enteredCommand = in.nextLine();
-            // if bye is entered, stop asking for commands and exit
-            if (enteredCommand.equals("bye")) {
+            userInput = in.nextLine();
+
+            if (userInput.equals("bye")) {
+                // if command "bye" is entered, stop asking for commands and exit
                 isBotRunning = false;
                 break;
-            }
-            // prints the task list if command "list" is given
-            else if (enteredCommand.equals("list")) {
+            } else if (userInput.equals("list")) {
+                // prints the task list if command "list" is given
                 System.out.println("____________________________________________________________");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(String.format("%d. [%s] %s", i + 1, taskList[i].getStatusIcon(), taskList[i].description));
+                    System.out.println(String.format("%d. [%s] %s", i + 1, taskList[i].getStatusIcon(),
+                            taskList[i].description));
                 }
                 System.out.println("____________________________________________________________");
-            }
-            // if task indicated as done, update it in the list
-            else if (enteredCommand.contains("done")) {
-                int donePosition = enteredCommand.indexOf("done");
-                int doneTaskIndex = Integer.parseInt(enteredCommand.substring(donePosition+5));
-                taskList[doneTaskIndex-1].markAsDone();
+            } else if (userInput.contains("done")) {
+                // if input contains "done", find the corresponding task and update its status in the list
+                int donePosition = userInput.indexOf("done");
+                int doneTaskIndex = Integer.parseInt(userInput.substring(donePosition + 5));
+                if (doneTaskIndex > taskCount) {
+                    // if task indicated exceeds current task list, indicate the error
+                    System.out.println("____________________________________________________________");
+                    System.out.println("That task is not on the list.");
+                    System.out.println("____________________________________________________________");
+                    continue;
+                }
+                Task doneTask = taskList[doneTaskIndex - 1];
+                doneTask.markAsDone();
 
                 System.out.println("____________________________________________________________");
                 System.out.println("I have noted the completion of this task: ");
-                System.out.println(String.format("  [%s] %s", taskList[doneTaskIndex-1].getStatusIcon(), taskList[doneTaskIndex-1].description));
+                System.out.println(String.format("  [%s] %s", doneTask.getStatusIcon(), doneTask.description));
                 System.out.println("____________________________________________________________");
-            }
-            // adds commands to the task list
-            else {
-                Task newTaskToAdd = new Task(enteredCommand);
+            } else {
+                // adds input as tasks to the task list
+                Task newTaskToAdd = new Task(userInput);
                 taskList[taskCount] = newTaskToAdd;
                 taskCount++;
 
                 System.out.println("____________________________________________________________");
-                System.out.println("I have added \"" + enteredCommand + "\" to your list of tasks.");
+                System.out.println("I have added \"" + userInput + "\" to your list of tasks.");
                 System.out.println("____________________________________________________________");
             }
         }
