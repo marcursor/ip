de078b8bd9707740726311e43ca7182f83eeb1e2
MarcusTan <marcus.tan.598@gmail.com>
2020-09-30 21:12:29 +0800

    Convert import to single imports

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
